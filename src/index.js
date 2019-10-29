e91eafff0739e76d242fb93b0224b50cdabfb9ad
Thiago Marinho <tgmarinho@gmail.com>
2019-10-29 10:38:39 -0400

    GoBarber Mobile - Aula 10 - Reactotron

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import './config/ReactotronConfig';
 import React from 'react';
 import { StatusBar } from 'react-native';
 import Routes from './routes';
+
+console.tron.log('Logando');
 
 export default function App() {
   return (
