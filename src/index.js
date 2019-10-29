60218e7bc2cd788644d4d60456e9d78eddfaf29f
Thiago Marinho <tgmarinho@gmail.com>
2019-10-29 15:24:17 -0400

    GoBarber Mobile - Aula 13 - Rota inicial

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import Routes from './routes';
 
 import { store, persistor } from './store';
 
-export default function App() {
+import App from './App';
+
+export default function Index() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-        <Routes />
+        <App />
       </PersistGate>
     </Provider>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,25 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 
-export default createAppContainer(
-  createSwitchNavigator({
-    SignIn,
-    SignUp,
-  })
-);
+import Dashboard from './pages/Dashboard';
+
+export default (isSigned = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        App: createBottomTabNavigator({
+          Dashboard,
+        }),
+      },
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      }
+    )
+  );
