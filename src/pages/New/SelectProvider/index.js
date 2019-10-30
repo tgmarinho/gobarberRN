b1be0ab3a050f22275852c8d4f36e059377602db
Thiago Marinho <tgmarinho@gmail.com>
2019-10-30 11:57:28 -0400

    GoBarber Mobile - Aula 19 - Rotas de Agendamentos

---
+++ src/pages/New/SelectProvider/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Background from '~/components/Background';
+
+// import { Container } from './styles';
+
+export default function SelectProvider() {
+  return <Background />;
+}
+
+SelectProvider.navigationOptions = ({ navigation }) => ({
+  title: 'Selecione o prestador',
+  headerLeft: () => (
+    <TouchableOpacity onPress={() => navigation.navigate('Dashboard')}>
+      <Icon name="chevron-left" size={20} color="#FFF" />
+    </TouchableOpacity>
+  ),
+});
